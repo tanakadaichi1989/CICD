4749b1a0be8fcba95b696ef6229d5bb037634126
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-26 23:42:58 +0900

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by 田中大地 on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
