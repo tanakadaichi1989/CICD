97067ec1b23b12bd7115dc304cf86d2700ca70fd
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-05-26 23:50:09 +0900

    Edit ContentView.swift

--- CICD/ContentView.swift
+++ CICD/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("CICD")
             .font(.largeTitle)
-            .fontWeight(.blackß)
+            .fontWeight(.black)
             .padding()
     }
 }
